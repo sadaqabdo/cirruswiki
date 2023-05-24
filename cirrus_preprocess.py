eb4f255e48dde2b489f63f630a30180d0fe61376
Abdo <73716075+sadaqabdo@users.noreply.github.com>
2023-05-24 11:44:22 +0200

    Update cirrus_preprocess.py

--- cirrus_preprocess.py
+++ cirrus_preprocess.py
@@ -126,7 +126,7 @@ class CirrusPreprocess:
                     except Exception as e:
                         raise (f"Error while writing to {export_pathfile}: {e}")
 
-                if len(doc_tracker) % 1_000_000 == 0:
+                if doc_tracker % 1_000_000 == 0:
                     print(f"Tokenized {doc_tracker} articles")
 
         print(
